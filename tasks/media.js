ffdba738fa74cbce223f532ff7bfee7734f396b1
clurts <clu@rts.dk>
2021-04-14 08:03:33 +0200

    add media.js

---
+++ tasks/media.js
@@ -0,0 +1,18 @@
+const gulp = require("gulp");
+const connect = require("gulp-connect");
+
+function media() {
+    return gulp.src("./src/media/**/*.*")
+        .pipe(gulp.dest("./dist/media"))
+        .pipe(connect.reload());
+}
+
+function watchMEDIA() {
+    return gulp.watch("./src/media/**/*.*", {
+        ignoreInitial: false
+    }, media)
+}
+
+module.exports = {
+    watchMEDIA
+}
